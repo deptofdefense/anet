26b1e91a4d941e01c5960dc8777c88ed8c30cce7
Nick Small <nick@dds.mil>
2016-12-07 15:51:04 -0500

    Revert "Move react src into main repo root"
    This reverts commit 3f3e2eb8021688cde955db1f4e8f1441ee5a6afc.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './pages/App'
+import App from './App'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
