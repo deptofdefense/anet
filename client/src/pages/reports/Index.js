2feaf6bb8c31c178450650342b21fa562aca87ba
Nick Small <nick@dds.mil>
2016-12-08 01:03:09 -0500

    Start populating reports#index view

--- client/src/pages/reports/Index.js
+++ client/src/pages/reports/Index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Table} from 'react-bootstrap'
+import {Link} from 'react-router'
 import API from '../../api'
 
 export default class ReportsIndex extends React.Component {
@@ -8,14 +10,38 @@ export default class ReportsIndex extends React.Component {
 	}
 
 	componentDidMount() {
-		API.fetch('/reports/1')
-			.then(data => this.setState({reports: data}))
+		API.fetch('/reports')
+			.then(data => this.setState({reports: data.list}))
+	}
+
+	renderTableRow(report) {
+		return (
+			<tr key={report.id}>
+				<td>{report.author.name}</td>
+				<td><Link to={{pathname: `/reports/${report.id}`}}>{report.intent}</Link></td>
+				<td>{report.state}</td>
+				<td>{report.updatedAt}</td>
+			</tr>
+		)
 	}
 
 	render() {
 		return (
 			<div>
-				{this.state.reports.state}
+				<Table striped>
+					<thead>
+						<tr>
+							<th>Reporter</th>
+							<th>Summary</th>
+							<th>Status</th>
+							<th>Last updated</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						{this.state.reports.map(this.renderTableRow)}
+					</tbody>
+				</Table>
 			</div>
 		)
 	}
