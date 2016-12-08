bbeb025a2e5702e349468afc38520fab7d6cb040
Nick Small <nick@dds.mil>
2016-12-08 00:49:20 -0500

    First pass at fetching data

--- client/src/pages/reports/Index.js
+++ client/src/pages/reports/Index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import API from '../../api'
 
 export default class ReportsIndex extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {reports: []}
+	}
+
+	componentDidMount() {
+		API.fetch('/reports/1')
+			.then(data => this.setState({reports: data}))
+	}
+
 	render() {
-		return <div>Reports!</div>
+		return (
+			<div>
+				{this.state.reports.state}
+			</div>
+		)
 	}
 }
