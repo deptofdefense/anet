c195854b7d24a740506c000a0681f907326f4dd7
Nick Small <nick@dds.mil>
2016-12-16 09:27:20 -0500

    Fill out a bit more of report#show

--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Form} from 'react-bootstrap'
+import {Link} from 'react-router'
 import moment from 'moment'
 
 import API from '../../api'
@@ -21,17 +22,37 @@ export default class ReportShow extends React.Component {
 		let report = this.state.report
 		let breadcrumbName = report.intent || 'Report'
 		let breadcrumbUrl = '/reports/' + (report.id || this.props.params.id)
+
+		let atmosphereString = report.atmosphere
+		if (report.atmosphereDetails) atmosphereString += ": " + report.atmosphereDetails
+
 		return (
 			<div>
 				<Breadcrumbs items={[['Reports', '/reports'], [breadcrumbName, breadcrumbUrl]]} />
 
-				<h2>Report #{report.id}</h2>
-
 				<Form horizontal>
 					<fieldset>
+						<legend>Report #{report.id}</legend>
 						<HorizontalFormField label="Subject" type="static" value={report.intent} />
 						<HorizontalFormField label="Date" type="static" value={moment(report.engagementDate).format("L LT")} />
-						<HorizontalFormField label="Location" type="static" value={report.engagementLocation} />
+						<HorizontalFormField label="Location" type="static" value={report.location && report.location.name} extra="📍"/>
+						<HorizontalFormField label="Atmospherics" type="static" value={atmosphereString} />
+						<HorizontalFormField label="Report author" type="static">
+							{report.author &&
+								<Link to={"/users/" + report.author.id}>{report.author.name}</Link>
+							}
+						</HorizontalFormField>
+					</fieldset>
+
+					<fieldset>
+						<legend>Meeting attendees</legend>
+						{report.attendees && report.attendees.map(person =>
+							{person.name}
+						)}
+					</fieldset>
+
+					<fieldset>
+						<legend>Milestones</legend>
 					</fieldset>
 				</Form>
 			</div>
