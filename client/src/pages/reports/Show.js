a76effa9de9c798bcdc087581a581994ecd7dea1
Nick Small <nickjsiii@gmail.com>
2016-12-19 17:30:04 -0500

    Load location data in a separate request if it hasn’t been loaded on report#show

--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -10,16 +10,28 @@ import {HorizontalFormField} from '../../components/FormField'
 export default class ReportShow extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {report: {}}
+		this.state = {
+			report: {},
+			location: {},
+		}
 	}
 
 	componentDidMount() {
 		API.fetch('/api/reports/' + this.props.params.id)
 			.then(data => this.setState({report: data}))
+			.then(() => {
+				let location = this.state.report.location
+				if (location && location.id && !location.name) {
+					API.fetch('/api/locations/' + location.id)
+						.then(data => this.setState({location: data}))
+				} else {
+					this.setState({location})
+				}
+			})
 	}
 
 	render() {
-		let report = this.state.report
+		let {report, location} = this.state
 		let breadcrumbName = report.intent || 'Report'
 		let breadcrumbUrl = '/reports/' + (report.id || this.props.params.id)
 
@@ -35,7 +47,7 @@ export default class ReportShow extends React.Component {
 						<legend>Report #{report.id}</legend>
 						<HorizontalFormField label="Subject" type="static" value={report.intent} />
 						<HorizontalFormField label="Date" type="static" value={moment(report.engagementDate).format("L LT")} />
-						<HorizontalFormField label="Location" type="static" value={report.location && report.location.name} extra="📍"/>
+						<HorizontalFormField label="Location" type="static" value={location && location.name} extra="📍"/>
 						<HorizontalFormField label="Atmospherics" type="static" value={atmosphereString} />
 						<HorizontalFormField label="Report author" type="static">
 							{report.author &&
