ebaef33953a98ed01d2c95a2171f7439ea5619f5
Nick Small <nickjsiii@gmail.com>
2016-12-19 16:39:01 -0500

    Send browser credentials with fetch instead of hard coding them

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
 export default {
 	fetch(url) {
 		return fetch(url, {
+			credentials: "same-origin",
 			headers: {
-				accept: "application/json",
-				authorization: "Basic " + btoa("nick:Nicholson")
+				accept: "application/json"
 			}
 		}).then(response => response.json())
 		  .then(function(data) {
