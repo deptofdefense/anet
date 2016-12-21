124bce0ee7a66a7939714a40a88c64351785e036
Nick Small <nick@dds.mil>
2016-12-20 23:28:51 -0500

    Switch reports/show to graphql

--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -7,36 +7,44 @@ import API from '../../api'
 import Breadcrumbs from '../../components/Breadcrumbs'
 import {HorizontalFormField} from '../../components/FormField'
 
+const atmosphereStyle = {
+	fontSize: '2rem',
+	display: 'inline-block',
+	marginTop: '-4px'
+}
+
+const atmosphereIcons = {
+	'POSITIVE': "👍",
+	'NEUTRAL': "😐",
+	'NEGATIVE': "👎",
+}
+
 export default class ReportShow extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 			report: {},
-			location: {},
 		}
 	}
 
 	componentDidMount() {
-		API.fetch('/api/reports/' + this.props.params.id)
-			.then(data => this.setState({report: data}))
-			.then(() => {
-				let location = this.state.report.location
-				if (location && location.id && !location.name) {
-					API.fetch('/api/locations/' + location.id)
-						.then(data => this.setState({location: data}))
-				} else {
-					this.setState({location})
-				}
-			})
+		API.query(`
+			report(id:${this.props.params.id}) {
+				id, intent, engagementDate, atmosphere, atmosphereDetails
+				reportText, nextSteps
+				location { id, name }
+				author { id, name }
+				attendees { id, name }
+				poams { id, shortName, longName }
+
+			}
+		`).then(data => this.setState({report: data.report}))
 	}
 
 	render() {
-		let {report, location} = this.state
+		let {report} = this.state
 		let breadcrumbName = report.intent || 'Report'
 		let breadcrumbUrl = '/reports/' + (report.id || this.props.params.id)
-
-		let atmosphereString = report.atmosphere
-		if (report.atmosphereDetails) atmosphereString += ": " + report.atmosphereDetails
 
 		return (
 			<div>
@@ -46,9 +54,12 @@ export default class ReportShow extends React.Component {
 					<fieldset>
 						<legend>Report #{report.id}</legend>
 						<HorizontalFormField label="Subject" type="static" value={report.intent} />
-						<HorizontalFormField label="Date" type="static" value={moment(report.engagementDate).format("L LT")} />
-						<HorizontalFormField label="Location" type="static" value={location && location.name} extra="📍"/>
-						<HorizontalFormField label="Atmospherics" type="static" value={atmosphereString} />
+						<HorizontalFormField label="Date 📆" type="static" value={moment(+report.engagementDate).format("L LT")} />
+						<HorizontalFormField label="Location 📍" type="static" value={report.location && report.location.name} />
+						<HorizontalFormField label="Atmospherics" type="static">
+							<span style={atmosphereStyle}>{atmosphereIcons[report.atmosphere]}</span>
+							{report.atmosphereDetails && " " + report.atmosphereDetails}
+						</HorizontalFormField>
 						<HorizontalFormField label="Report author" type="static">
 							{report.author &&
 								<Link to={"/users/" + report.author.id}>{report.author.name}</Link>
