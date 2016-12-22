639cc11562e365b5ab7d44eff3b74980a511d033
Nick Small <nick@dds.mil>
2016-12-22 01:23:18 -0500

    Fetch organizations through gql and show them in the Nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import {Nav, NavItem} from 'react-bootstrap'
+import {Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
 import {IndexLinkContainer as Link} from 'react-router-bootstrap'
 
+import API from '../api'
+
 export default class extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {organizations: []}
+	}
+
+	componentDidMount() {
+		API.query(`
+			organizations(f:getAllOrgs, pageNum:0, pageSize:200) {
+				id, name
+				parentOrg { id }
+			}
+		`).then(data => this.setState({organizations: data.organizations.filter(org => org.parentOrg === null)}))
+	}
+
 	render() {
 		return (
 			<Nav bsStyle="pills" stacked>
@@ -15,12 +31,23 @@ export default class extends React.Component {
 				</Link>
 
 				<Link to="/reports">
-					<NavItem>Reports</NavItem>
+					<NavItem>My Reports</NavItem>
 				</Link>
 
-				{process.env.NODE_ENV === 'development' && <Link to="/graphiql">
-					<NavItem>GraphQL</NavItem>
-				</Link>}
+				<NavDropdown title="Organizations" id="organizations">
+					{this.state.organizations.map(org =>
+						<Link to={"/organizations/" + org.id} key={org.id}>
+							<MenuItem>{org.name}</MenuItem>
+						</Link>
+					)}
+				</NavDropdown>
+
+
+				{process.env.NODE_ENV === 'development' &&
+					<Link to="/graphiql">
+						<NavItem>GraphQL</NavItem>
+					</Link>
+				}
 			</Nav>
 		)
 	}
