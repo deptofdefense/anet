a2efa468f593c3f25375c7697d3746cea8c1d1ac
Nick Small <nick@dds.mil>
2016-12-22 01:57:42 -0500

    Add decorator support and autobind-decorators

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -107,6 +107,7 @@ module.exports = {
         include: paths.appSrc,
         loader: 'babel',
         query: {
+          plugins: ['transform-decorators-legacy'],
 
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/react-scripts/
--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -111,7 +111,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-
+        query: {
+          plugins: ['transform-decorators-legacy']
+        }
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
