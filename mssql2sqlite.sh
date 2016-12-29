1d50ac4b80e70fb6c079ff19872da288ba81d023
Nick Small <nick@dds.mil>
2016-12-29 12:36:45 -0500

    Add macOS support to mssql2sqlite.sh

--- mssql2sqlite.sh
+++ mssql2sqlite.sh
@@ -1,13 +1,25 @@
 
-function dtg { 
+function dtg_linux {
 	a=`date +%Y-%m-%d\ %H:%M:%S.`
 	b=`date +%N | cut -c1,2,3`
 	c=`date +\ %z`
 	echo "${a}${b}${c}"
 }
 
-while read input 
+function dtg_mac {
+	a=`date +%Y-%m-%d\ %H:%M:%S.`
+	b=`perl -MTime::HiRes -e 'printf("%.0f\n",Time::HiRes::time()*1000)' | cut -c11,12,13`
+	c=`date +\ %z`
+	echo "${a}${b}${c}"
+}
+
+while read input
 do
-	time=`dtg`;
-	echo ${input} | sed "s/CURRENT_TIMESTAMP/'${time}'/g" | sed -r "s/'([0-9]{4}-[0-9]{2}-[0-9]{2})'/'\1 00:00:00.000 -0000'/g" | sed "s/TRUNCATE TABLE/DELETE FROM/"
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		time=`dtg_mac`;
+		echo ${input} | sed "s/CURRENT_TIMESTAMP/'${time}'/g" | sed -E "s/'([0-9]{4}-[0-9]{2}-[0-9]{2})'/'\1 00:00:00.000 -0000'/g" | sed "s/TRUNCATE TABLE/DELETE FROM/"
+	else
+		time=`dtg_linux`;
+		echo ${input} | sed "s/CURRENT_TIMESTAMP/'${time}'/g" | sed -r "s/'([0-9]{4}-[0-9]{2}-[0-9]{2})'/'\1 00:00:00.000 -0000'/g" | sed "s/TRUNCATE TABLE/DELETE FROM/"
+	fi
 done
