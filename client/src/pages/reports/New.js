e08b8e68237ef1e75a8c33bb3097d6f4bd480117
Nick Small <nick@dds.mil>
2016-12-29 22:21:48 -0500

    Have Form automatically add submit buttons to Header and bottom of forms that have an onSubmit prop

--- client/src/pages/reports/New.js
+++ client/src/pages/reports/New.js
@@ -71,7 +71,7 @@ export default class ReportNew extends React.Component {
 
 				<Breadcrumbs items={[['EF4', '/organizations/ef4'], ['Submit a report', '/reports/new']]} />
 
-				<Form formFor={report} onChange={this.onChange} onSubmit={this.onSubmit} horizontal>
+				<Form formFor={report} onChange={this.onChange} onSubmit={this.onSubmit} actionText="Create report" horizontal>
 					{this.state.error && <fieldset><p>There was a problem saving this report.</p><p>{this.state.error}</p></fieldset>}
 					<fieldset>
 						<legend>Engagement details <small>Required</small></legend>
@@ -186,10 +186,6 @@ export default class ReportNew extends React.Component {
 						<Form.Field id="nextSteps" label="" horizontal={false} style={{marginTop: '5rem'}}>
 							<TextEditor label="Next steps" />
 						</Form.Field>
-					</fieldset>
-
-					<fieldset>
-						<Button bsSize="large" bsStyle="primary" type="submit" className="pull-right">Create report</Button>
 					</fieldset>
 				</Form>
 			</div>
