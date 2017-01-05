a7bc4f729ace240e94dfb79760b1b95e8f25858c
Nick Small <nick@dds.mil>
2017-01-05 02:17:08 -0500

    Only show create actions currentUser is authorized for

--- client/src/components/CreateButton.js
+++ client/src/components/CreateButton.js
@@ -3,26 +3,35 @@ import {DropdownButton, MenuItem} from 'react-bootstrap'
 import History from 'components/History'
 import * as Models from 'models'
 
-const RESOURCES = [
-	Models.Report,
-	Models.Person,
-	Models.Position,
-	Models.Organization,
-	Models.Poam,
-]
-
 export default class CreateButton extends Component {
+	static contextTypes = {
+		app: React.PropTypes.object,
+	}
+
 	render() {
+		const currentUser = this.context.app.state.currentUser
+		const MODEL_CLASSES = [
+			Models.Report,
+		]
+
+		if (currentUser.isSuperUser()) {
+			MODEL_CLASSES.push(Models.Person, Models.Position, Models.Poam)
+		}
+
+		if (currentUser.isAdmin()) {
+			MODEL_CLASSES.push(Models.Organization)
+		}
+
 		return (
 			<DropdownButton title="Create" bsStyle="primary" id="createButton" onSelect={this.onSelect}>
-				{RESOURCES.map((resource, i) =>
-					<MenuItem key={resource.name} eventKey={i}>Create {resource.name}</MenuItem>
+				{MODEL_CLASSES.map((modelClass, i) =>
+					<MenuItem key={modelClass.name} eventKey={modelClass}>Create {modelClass.name}</MenuItem>
 				)}
 			</DropdownButton>
 		)
 	}
 
-	onSelect(index) {
-		History.push(RESOURCES[index].pathFor(null))
+	onSelect(modelClass) {
+		History.push(modelClass.pathFor(null))
 	}
 }
