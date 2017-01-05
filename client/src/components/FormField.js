533977b28d9c0fe1e7874adfc6694fa5b20ffb19
Nick Small <nick@dds.mil>
2017-01-05 02:36:16 -0500

    Fix organization/Show throwing errors about nesting inside of p tags

--- client/src/components/FormField.js
+++ client/src/components/FormField.js
@@ -82,7 +82,7 @@ export default class FormField extends Component {
 
 		// if type is static, render out a static value
 		if (this.props.type === 'static' || (!this.props.type && this.context.form.props.static)) {
-			children = <FormControl.Static {...childProps}>{(children.length && children) || defaultValue}</FormControl.Static>
+			children = <FormControl.Static componentClass={'div'} {...childProps}>{(children.length && children) || defaultValue}</FormControl.Static>
 
 		// if children are provided, render those, but special case them to
 		// automatically set value and children props
