d865e52ffe9cdfebc5643dfdbbb6b62de9463c0c
Nick Small <nick@dds.mil>
2017-01-15 11:10:24 -0500

    Add first report test

---
+++ client/src/pages/__tests__/reports.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReportNew from 'pages/reports/New';
+
+it('reports/new renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ReportNew />, div);
+});
