945ea8fcf1c4396738d6cebe847dd3256a589255
Nick Small <nick@dds.mil>
2017-01-18 13:54:39 -0500

    Lots of cleanup for rollup, render d3 chart

--- client/src/pages/rollup/Show.js
+++ client/src/pages/rollup/Show.js
@@ -1,86 +1,75 @@
 import React from 'react'
 import Page from 'components/Page'
-import {DropdownButton, MenuItem} from 'react-bootstrap'
-
-import Breadcrumbs from 'components/Breadcrumbs'
-import Form from 'components/Form'
-import LinkTo from 'components/LinkTo'
 import autobind from 'autobind-decorator'
+
+import {DropdownButton, MenuItem} from 'react-bootstrap'
+import Breadcrumbs from 'components/Breadcrumbs'
 import History from 'components/History'
-import moment from 'moment'
 
 import API from 'api'
-import {Poam} from 'models'
+import {Report} from 'models'
+
+import moment from 'moment'
 import * as d3 from 'd3'
 
 export default class RollupShow extends Page {
+	static propTypes = {
+		date: React.PropTypes.object,
+	}
+
+	static defaultProps = {
+		date: moment(),
+	}
+
 	static contextTypes = {
 		app: React.PropTypes.object.isRequired,
 	}
-	static propTypes = {
-		date : React.PropTypes.object
-	}
-	static stateTypes = {
-		reports : React.PropTypes.object
-	}
-	static defaultProps = {
-		date : moment()
-	}
-	constructor(props) {
-		super(props)
-		this.state = {
-			date : {},
-			reports : {}
-		}
-	}
-	get dateStr(){ return this.props.date.format('YYYY-MM-DD') }
+
+	get dateStr() { return this.props.date.format('YYYY-MM-DD') }
 	get dateLongStr() { return this.props.date.format('MMMM DD, YYYY') }
 	get rollupStart() { return this.props.date.startOf('day') }
 	get rollupEnd() { return this.props.date.endOf('day') }
 
+	constructor(props) {
+		super(props)
+		this.state = {
+			date: {},
+			reports: {},
+		}
+	}
+
 	fetchData(props) {
 		// TODO: this is a hack to make sure we get some data, I am not using the 
 		API.query(/* GraphQL */`
-				{
-  reports(f:getAll,pageSize:20,pageNum:0){
-    state
-    intent
-    advisorOrg {
-      id
-      shortName
-      parentOrg {
-        id
-        shortName
-      }
-    }
-  }
-}
-`)
+			reports(f:getAll,pageSize:20,pageNum:0) {
+				id, state, intent
+				advisorOrg {
+					id, shortName
+					parentOrg { id, shortName }
+				}
+			}
+		`).then(data => {
+			this.setState({reports: Report.fromArray(data.reports)})
+		})
 	}
 
-	render() {
-		let {reports} = this.state
-		// Admins can edit poams, or super users if this poam is assigned to their org.
-		let currentUser = this.context.app.state.currentUser
-		let canEdit = (currentUser && currentUser.isAdmin())
-			// || (currentUser && poam.responsibleOrg && currentUser.isSuperUserForOrg(poam.responsibleOrg));
+	componentDidMount() {
+		super.componentDidMount()
+		this.componentDidUpdate()
+	}
 
-		// let dateStrS = moment().format('YYYY-MM-DD')
-		
+	componentDidUpdate() {
+		let {reports} = this.state
+		if (!reports)
+			return
+
 		// Sets up the data
 		var step1 = d3.nest()
 				.key(function(d){return d.advisorOrg.parentOrg.shortName;})
 				.rollup(function(d){return {l:d.length,s:d[0].state,r:d}})
 				.entries(reports)
 
-				// TODO: this is imperative, should probably use 
-				// something like
-				// var svg = ReactFauxDOM.createElement('ul')
-				// d3.select(svg)
-				// ... lots of code
-				// var reactDOM =  list.toReact()
-				// http://oli.me.uk/2015/09/09/d3-within-react-the-right-way/
-		var svg = d3.select("svg"), 
+		var svg = d3.select(this.graph),
 			margin = {top: 20, right: 20, bottom: 20, left: 20},
 			width = +svg.attr("width") - margin.left - margin.right,
 			height = +svg.attr("height") - margin.top - margin.bottom,
@@ -94,7 +83,7 @@ export default class RollupShow extends Page {
 
 		x.domain(step1.map(function(d){return d.key}));
 		y.domain([0,d3.max(step1.map(function(d){return d.value.l}))]);
-		var line = d3.line()
+		d3.line()
 			.x(function(d,i) { return x(d.key); })
 			.y(function(d,i) { return y(d.value); });
 
@@ -126,16 +115,23 @@ export default class RollupShow extends Page {
 			.data(step1)
 			.enter().append("rect")
 			.attr("class", "line")
-			.attr("width", width / step1.length - padding )
+			.attr("width", width / step1.length - padding)
 			.attr("x",function(d,i){return x(d.key) + (width / (step1.length + padding))})
 			.attr("height", function(d,i){return height - y(d.value.l)})
 			.attr("y",function(d,i){return y(d.value.l) })
-			.attr("fill", function(d){return "green";})
-		;
+			.attr("fill", function(d){return "green"})
+	}
+
+	render() {
+		let {reports} = this.state
+
+		// Admins can edit poams, or super users if this poam is assigned to their org.
+		let currentUser = this.context.app.state.currentUser
+		let canEdit = currentUser && currentUser.isAdmin()
 
 		return (
 			<div>
-				<Breadcrumbs items={[['Rollup',''],[this.dateStr,'rollup/' +this.dateStr]]} />
+				<Breadcrumbs items={[['Rollup', ''],[this.dateStr, 'rollup/' +this.dateStr]]} />
 
 				{canEdit &&
 					<div className="pull-right">
@@ -145,26 +141,22 @@ export default class RollupShow extends Page {
 					</div>
 				}
 
-				<Form static formFor={reports} horizontal>
-					<fieldset>
-						<legend>Daily Rollup - {this.dateLongStr}</legend>
-						<div className="todo" contentEditable suppressContentEditableWarning>Some introductory text regarding {reports && reports.reports && reports.reports.length} reports, using 230 days due to SQLite issues.</div>
-					</fieldset>
-					<fieldset>
-						<legend>Summary of Report Input</legend>
-						<svg>
-						</svg>
-						<div className="todo" contentEditable suppressContentEditableWarning>Pretty graphs here</div>
-					</fieldset>
-					<fieldset>
-						<legend>Report of the Day</legend>
-						<div className="todo" contentEditable suppressContentEditableWarning>Pretty graphs here</div>
-					</fieldset>
-					<fieldset>
-						<legend>Reports - {this.dateLongStr}</legend>
-						<div className="todo" contentEditable suppressContentEditableWarning>Pretty graphs here</div>
-					</fieldset>
-				</Form>
+				<fieldset>
+					<legend>Daily Rollup - {this.dateLongStr}</legend>
+					<div className="todo" contentEditable suppressContentEditableWarning>Some introductory text regarding {reports && reports.reports && reports.reports.length} reports, using 230 days due to SQLite issues.</div>
+				</fieldset>
+				<fieldset>
+					<legend>Summary of Report Input</legend>
+					<svg ref={el => this.graph = el} />
+				</fieldset>
+				<fieldset>
+					<legend>Report of the Day</legend>
+					<div className="todo" contentEditable suppressContentEditableWarning>Pretty graphs here</div>
+				</fieldset>
+				<fieldset>
+					<legend>Reports - {this.dateLongStr}</legend>
+					<div className="todo" contentEditable suppressContentEditableWarning>Pretty graphs here</div>
+				</fieldset>
 			</div>
 		)
 	}
