4403083c959378ad154614ec33f0e35519b411db
Nick Small <nick@dds.mil>
2017-01-26 17:56:10 -0500

    Shorten Next Step and Key Outcome summary fields

--- client/src/pages/reports/Form.js
+++ client/src/pages/reports/Form.js
@@ -31,9 +31,7 @@ export default class ReportForm extends Component {
 				poams: [],
 			},
 
-			showKeyOutcomesText: false,
-			showNextStepsText: false,
-			showReportText: false
+			showReportText: false,
 		}
 	}
 
@@ -73,7 +71,7 @@ export default class ReportForm extends Component {
 
 		return <Form formFor={report} horizontal onChange={this.onChange} onSubmit={this.onSubmit} submitText="Save report">
 			<fieldset>
-				<legend>Engagement Details<small>Required</small></legend>
+				<legend>Engagement Details <small>Required</small></legend>
 
 				<Form.Field id="intent" label="Meeting purpose" placeholder="What happened?" data-focus>
 					<Form.Field.ExtraCol>{250 - report.intent.length} characters remaining</Form.Field.ExtraCol>
@@ -166,37 +164,17 @@ export default class ReportForm extends Component {
 
 			<fieldset>
 				<legend>Meeting Discussion <small>Required</small></legend>
-				<Form.Field id="keyOutcomesSummary">
-					<Form.Field.ExtraCol>{250 - report.keyOutcomesSummary.length} characters remaining</Form.Field.ExtraCol>
+
+				<Form.Field id="keyOutcomes">
+					<Form.Field.ExtraCol><small>{250 - report.keyOutcomes.length}</small></Form.Field.ExtraCol>
 				</Form.Field>
-				<Button bsStyle="link" onClick={this.toggleKeyOutcomesText}>
-					{this.state.showKeyOutcomesText ? "Hide" : "Add" } details to Key Outcomes
-				</Button>
 
-				<Collapse in={this.state.showKeyOutcomesText}>
-					<div>
-						<Form.Field id="keyOutcomes" label="" horizontal={false}>
-							<TextEditor label="Key outcomes" />
-						</Form.Field>
-					</div>
-				</Collapse>
-
-				<Form.Field id="nextStepsSummary">
-					<Form.Field.ExtraCol>{250 - report.nextStepsSummary.length} characters remaining</Form.Field.ExtraCol>
+				<Form.Field id="nextSteps">
+					<Form.Field.ExtraCol><small>{250 - report.nextSteps.length}</small></Form.Field.ExtraCol>
 				</Form.Field>
-				<Button bsStyle="link" onClick={this.toggleNextStepsText}>
-					{this.state.showNextStepsText ? "Hide" : "Add" } details to Next Steps
-				</Button>
-				<Collapse in={this.state.showNextStepsText}>
-					<div>
-						<Form.Field id="nextSteps" label="" horizontal={false} style={{marginTop: '5rem'}}>
-							<TextEditor label="Next steps" />
-						</Form.Field>
-					</div>
-				</Collapse>
 
-				<Button bsStyle="link" onClick={this.toggleReportText} >
-					{this.state.showReportText ? "Hide" : "Add" } detailed comments
+				<Button className="center-block toggle-section-button" onClick={this.toggleReportText}>
+					{this.state.showReportText ? "Hide" : "Add"} detailed comments
 				</Button>
 
 				<Collapse in={this.state.showReportText}>
@@ -206,19 +184,8 @@ export default class ReportForm extends Component {
 						</Form.Field>
 					</div>
 				</Collapse>
-
 			</fieldset>
 		</Form>
-	}
-
-	@autobind
-	toggleKeyOutcomesText() {
-		this.setState({showKeyOutcomesText: !this.state.showKeyOutcomesText})
-	}
-
-	@autobind
-	toggleNextStepsText() {
-		this.setState({showNextStepsText: !this.state.showNextStepsText})
 	}
 
 	@autobind
