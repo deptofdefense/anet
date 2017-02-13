4ba13147b906c122c118a48362a3830c9a636a71
Nick Heiner <nth23@cornell.edu>
2017-02-13 17:32:02 -0500

    Working in window.ANET_DATA but it does not appear to be of the right form.

--- client/src/pages/onboarding/Show.js
+++ client/src/pages/onboarding/Show.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Page from 'components/Page'
 import {Button} from 'react-bootstrap'
+import autobind from 'autobind-decorator'
+import History from 'components/History'
+import {Person} from 'models'
 
 export default class OnboardingShow extends Page {
     static pageProps = {
@@ -10,7 +13,9 @@ export default class OnboardingShow extends Page {
 
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = {
+            currentUser: new Person(window.ANET_DATA.currentUser) 
+        }
 	}
 
 	render() {
@@ -18,9 +23,9 @@ export default class OnboardingShow extends Page {
             <div className="onboarding-new">
 			    <h1>Welcome to ANET</h1>
                 <p>ANET is an information system for reporting on TAA engagements, and learning about past engagements and people.</p>
-                <p>Let's create a new account for your as an ADVISOR. We'll get your basic information that will allow your supervisor to approve your account.</p>
+                <p>Let's create a new account for your as an <span className="role">{this.state.currentUser.role}</span>. We'll get your basic information that will allow your supervisor to approve your account.</p>
                 <div className="create-account-button-wrapper">
-                    <Button bsStyle="primary">Create your account</Button>
+                    <Button bsStyle="primary" onClick={this.onCreateAccountClick}>Create your account</Button>
                 </div>
                 <div className="help">
                     <p>Concerned or stuck?</p>
@@ -30,4 +35,9 @@ export default class OnboardingShow extends Page {
 			</div>
 		)
 	}
+
+    @autobind
+    onCreateAccountClick() {
+        History.push(Person.pathForEdit(this.state.currentUser))
+    }
 }
