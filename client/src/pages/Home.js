dc6a66fec1b6ce5ce78cc49e388af00811b607b9
Nick Heiner <nth23@cornell.edu>
2017-03-08 10:57:09 -0500

    Undo change just for PR.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,7 +24,7 @@ export default class Home extends HopscotchPage {
 			upcomingEngagements: null,
 			savedSearches: [],
 			selectedSearch: null,
-			showGettingStartedPanel: 'true' /* just for PR */
+			showGettingStartedPanel: window.localStorage.showGettingStartedPanel
 		}
 	}
 
