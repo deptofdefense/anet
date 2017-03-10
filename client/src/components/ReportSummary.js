08d9cb39facab9e4d816f7ad1309dab125157e1c
Nick Heiner <nth23@cornell.edu>
2017-03-10 11:47:41 -0500

    tweaking the people separator

--- client/src/components/ReportSummary.js
+++ client/src/components/ReportSummary.js
@@ -50,12 +50,12 @@ export default class ReportSummary extends Component {
 				<Col md={6}>
 					<PersonComponent person={report.primaryAdvisor} />
 					{report.advisorOrg &&
-						<span>&nbsp;(<LinkTo organization={report.advisorOrg} />)</span>
+						<span> (<LinkTo organization={report.advisorOrg} />)</span>
 					}
-					&#x25B6;
+					<span className="people-separator">&#x25B6;</span>
 					<PersonComponent person={report.primaryPrincipal} />
 					{report.principalOrg &&
-						<span>&nbsp;(<LinkTo organization={report.principalOrg} />)</span>
+						<span> (<LinkTo organization={report.principalOrg} />)</span>
 					}
 				</Col>
 			</Row>
