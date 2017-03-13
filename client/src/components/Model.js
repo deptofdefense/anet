628434f769a82c4251a97594e50399bb91e626cb
Nick Small <nick@dds.mil>
2017-03-13 22:23:09 +0430

    Allow passing query objects to Model path methods

--- client/src/components/Model.js
+++ client/src/components/Model.js
@@ -1,5 +1,13 @@
 import utils from 'utils'
 
+function buildQueryString(query) {
+	let params = []
+	Object.forEach(query, (key, value) => {
+		params.push(`${key}=${encodeURIComponent(value)}`)
+	})
+	return params.join('&')
+}
+
 export default class Model {
 	static schema = {}
 
@@ -29,7 +37,7 @@ export default class Model {
 		)
 	}
 
-	static pathFor(instance) {
+	static pathFor(instance, query) {
 		if (!instance)
 			return console.error(`You didn't pass anything to ${this.name}.pathFor. If you want a new route, you can pass null.`)
 
@@ -40,16 +48,34 @@ export default class Model {
 
 		let resourceName = utils.resourceize(this.resourceName)
 		let id = instance.id
-		return ['', resourceName, id].join('/')
+		let url = ['', resourceName, id].join('/')
+
+		if (query) {
+			url += '?' + buildQueryString(query)
+		}
+
+		return url
 	}
 
-	static pathForNew() {
+	static pathForNew(query) {
 		let resourceName = utils.resourceize(this.resourceName)
-		return ['', resourceName, 'new'].join('/')
+		let url = ['', resourceName, 'new'].join('/')
+
+		if (query) {
+			url += '?' + buildQueryString(query)
+		}
+
+		return url
 	}
 
-	static pathForEdit(instance) {
-		return this.pathFor(instance) + '/edit'
+	static pathForEdit(instance, query) {
+		let url = this.pathFor(instance) + '/edit'
+
+		if (query) {
+			url += '?' + buildQueryString(query)
+		}
+
+		return url
 	}
 
 	static isEqual(a, b) {
@@ -81,8 +107,8 @@ export default class Model {
 		return this
 	}
 
-	toPath() {
-		return this.id ? this.constructor.pathFor(this) : this.constructor.pathForNew()
+	toPath(query) {
+		return this.id ? this.constructor.pathFor(this, query) : this.constructor.pathForNew(query)
 	}
 
 	toString() {
