8d177d6f47e4a7d32c65e83e5d61a1538e019579
Nick Heiner <nth23@cornell.edu>
2017-03-15 18:29:23 -0400

    Trying to make pagination work

--- client/src/pages/reports/MyReports.js
+++ client/src/pages/reports/MyReports.js
@@ -7,6 +7,7 @@ import _get from 'lodash.get'
 import _mapValues from 'lodash.mapvalues'
 import Report from 'models/Report'
 import Fieldset from 'components/Fieldset'
+import autobind from 'autobind-decorator'
 
 export default class MyReports extends Page {
     constructor() {
@@ -58,24 +59,54 @@ export default class MyReports extends Page {
         )
     }
 
+    @autobind
+    queryReportPage(reportGroupName, $state, $pageNum) {
+        API.query(/* GraphQL */`
+			person(f:me) {
+				authoredReports(pageNum: $pageNum, pageSize: 10, state: [$state]) { 
+                    pageNum, pageSize, totalCount, list {
+                        id, intent, engagementDate, keyOutcomes, nextSteps, atmosphere
+                        primaryAdvisor { id, name } ,
+                        primaryPrincipal {id, name },
+                        advisorOrg { id, shortName, longName }
+                        principalOrg { id, shortName, longName }
+                        location { id, name, lat, lng }
+                    }
+                }
+			}
+		`, {$state, $pageNum}).then(data => 
+            this.setState({
+                reports: {
+                    [reportGroupName]: ({
+                        list: Report.fromArray(data.person.authoredReports.list), 
+                        ...data.person.authoredReports
+                    }),
+                    ...this.state.reports
+                }
+            })
+        )
+    }
+
     render() {
+        const ReportSection = props => {
+            let content = <p>Loading...</p>
+            const reportGroup = _get(props, ['reports', props.reportGroupName]),
+                goToPage = pageNum => this.queryReportPage(props.reportGroupName, props.reportGroupState, pageNum)
+
+            if (reportGroup) {
+                content = <ReportCollection paginatedReports={reportGroup} goToPage={goToPage} />
+            }
+
+            return <Fieldset title={props.title}>
+                {content}
+            </Fieldset>
+        }
+
         return <div>
             <Breadcrumbs items={[['My Reports', window.location.pathname]]} />
-            <ReportSection title="Draft Reports" reports={this.state.reports} reportGroup={'draft'} />
-            <ReportSection title="Pending Approval" reports={this.state.reports} reportGroup={'pending'} />
-            <ReportSection title="Published Reports" reports={this.state.reports} reportGroup={'released'} />
+            <ReportSection title="Draft Reports" reports={this.state.reports} reportGroupName='draft' reportGroupState='DRAFT' />
+            <ReportSection title="Pending Approval" reports={this.state.reports} reportGroupName='pending' reportGroupState='PENDING' />
+            <ReportSection title="Published Reports" reports={this.state.reports} reportGroupName='released' reportGroupState='RELEASED' />
         </div>
     }
-}
-
-function ReportSection(props) {
-    let content = <p>Loading...</p>
-    const reportGroup = _get(props, ['reports', props.reportGroup])
-    if (reportGroup) {
-        content = <ReportCollection paginatedReports={reportGroup} />
-    }
-
-    return <Fieldset title={props.title}>
-        {content}
-    </Fieldset>
 }
