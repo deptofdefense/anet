6079fadb781013dd94baadf3ba2f2b857ea620d9
Nick Heiner <nth23@cornell.edu>
2017-03-15 16:40:14 -0400

    Getting some reports onto the page

--- client/src/pages/reports/MyReports.js
+++ client/src/pages/reports/MyReports.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Page from 'components/Page'
 import Breadcrumbs from 'components/Breadcrumbs'
+import ReportCollection from 'components/ReportCollection'
+import API from 'api'
+import _get from 'lodash.get'
+import Report from 'models/Report'
 
 export default class MyReports extends Page {
     constructor() {
@@ -8,9 +12,35 @@ export default class MyReports extends Page {
         this.state = {}
     }
 
+    fetchData() {
+        API.query(/* GraphQL */`
+			person(f:me) {
+				authoredReports(pageNum:0, pageSize:10) { 
+                    list {
+                        id, intent, engagementDate, keyOutcomes, nextSteps
+                        primaryAdvisor { id, name } ,
+                        primaryPrincipal {id, name },
+                        advisorOrg { id, shortName, longName }
+                        principalOrg { id, shortName, longName }
+                        location { id, name, lat, lng }
+                    }
+                }
+			}
+		`).then(data => this.setState({reports: data.person.authoredReports.list.map(report => new Report(report))}))
+    }
+
     render() {
         return <div>
             <Breadcrumbs items={[['My Reports', window.location.pathname]]} />
+            <ReportSection title="Draft Reports" reports={this.state.reports} />
         </div>
     }
+}
+
+function ReportSection(props) {
+    return <div>
+        <h2>{props.title}</h2>
+        <ReportCollection reports={props.reports} />
+    </div>
+
 }
