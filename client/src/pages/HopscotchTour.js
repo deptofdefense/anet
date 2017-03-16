d7ec386591665da8219abf3c5af7f9e7266b3fc8
Nick Small <nick@dds.mil>
2017-03-16 11:13:26 +0430

    org/Show guided tour for super users

--- client/src/pages/HopscotchTour.js
+++ client/src/pages/HopscotchTour.js
@@ -160,8 +160,44 @@ let superUserTour = function(currentUser) { return {
 			onNext: () => History.push(Organization.pathFor(currentUser.position.organization), {continuingHopscotchTour: true})
 		},
 		{
-			title: 'New Report',
-			content: 'Create a report by clicking on this button. If you are a Super User you may have different options.',
+			title: 'Your organization\'s information',
+			content: 'In this section of the page, you will find general information about your organization. This includes a description of your organization, a list of who the super users are, and sub-organizations. If you\'re a super user for a parent EF or organization -- such as EF 1 rather than EF 1.1 -- you can make changes to your organization and any sub-organization. If you\'re a super user in a sub-organization, you can only make changes within that sub-organization.',
+			target: 'info',
+			placement: 'bottom',
+		},
+		{
+			title: 'Supported positions',
+			content: 'The "Support Positions" section shows the positions in your organization that currently have people assigned to them. The billet column tells you the position name of the position. The billet\'s code is at the end of the position name. If any of the information here looks wrong, click on the position name. You\'ll see a detailed view of that position, and will have the option to make changes. You can also take a guided tour of that page when you\'re on it.',
+			target: '#supportedPositions h2',
+			placement: 'top',
+		},
+		{
+			title: 'Vacant positions',
+			content: 'This section of the page will show you positions in your organizations that currently do not have anyone assigned to them. To assign someone to the position, or to mark the position inactive, you can click on the position name, and select the "Edit" button near the top-right of that page.',
+			target: '#vacantPositions h2',
+			placement: 'top',
+		},
+		{
+			title: 'Approval process',
+			content: 'Here, you can review the approval process for reports authored in your organization. If there\'s no approval chain set up, scroll to the top of the page and click the "Edit" button. You\'ll be able to add and name as many approval steps as you\'d like. There are a couple of important things to keep in mind when you\'re setting up approval chains. The first is that we recommend having more than one approver in each step. That means that more than one person can approve reports at that step. The second thing to keep in mind is that when a report has been approved by someone at each step in your approval chain, it will automatically go into that day\'s daily rollup. Lastly, you can add any one to your approval chain, they do not need to be a super user.',
+			target: '#approvals h2',
+			placement: 'top',
+		},
+		{
+			title: 'PoAMs',
+			content: 'The PoAMs or Pillars that your organization is responsible for will be displayed in this section. If you need to make changes, or if PoAMs change, please contact an administrator to update them.',
+			target: '#poams h2',
+			placement: 'top',
+		},
+		{
+			title: 'Your orginization\'s reports',
+			content: 'Here, you\'ll find the complete list of all reports authored by your members of your organization.',
+			target: '#reports h2',
+			placement: 'top',
+		},
+		{
+			title: 'Create',
+			content: 'Those are the main areas you\'ll need to check as a super user. When reviewing your updating your organization\'s information, you might need to create new people or positions. New members of your team will automatically create new accounts when they log in for the first time, unless they are moving to ANET 2 from ANET 1. In those cases, they should already have an account. If you do need to create a new report, person, position, or location, click on this "Create" button and select what you need from the menu. Each of those pages will have their own, quick tours which will explain what you need to do to fill out those pages.',
 			target: 'createButton',
 			placement: 'left',
 			fixedElement: true,
