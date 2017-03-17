dea9c155b3ddc0a6737c3fdcdcfd25f01f83fd24
Nick Small <nick@dds.mil>
2017-03-17 21:17:23 +0430

    Death Star

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react'
 
-import PLANET from 'resources/planet.png'
-
 export default class NotFound extends Component {
     componentWillMount() {
         document.getElementsByTagName('html')[0].classList.add('not-found')
@@ -10,11 +8,10 @@ export default class NotFound extends Component {
     componentWillUnmount() {
         document.getElementsByTagName('html')[0].classList.remove('not-found')
     }
-    
+
 	render() {
 		return <div>
             <h1 className="not-found-text">{this.props.text}</h1>
-            <img src={PLANET} className="planet" role="presentation" />
         </div>
 	}
 }
