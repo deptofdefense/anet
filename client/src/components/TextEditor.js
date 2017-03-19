e8bff29d4122bd0a8175c8dbd28685aa90448e09
Nick Small <nick@dds.mil>
2017-03-19 18:08:53 +0430

    Remove link and twitter alloy buttons, add ul, ol

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -3,9 +3,18 @@ import React, {Component} from 'react'
 import AlloyEditor from 'alloyeditor/dist/alloy-editor/alloy-editor-no-react'
 import 'alloyeditor/dist/alloy-editor/assets/alloy-editor-atlas.css'
 
+AlloyEditor.Selections[3].buttons.splice(4, 2)
+
+const ALLOY_CONFIG = {
+	toolbars: {
+		styles: { selections: AlloyEditor.Selections },
+		add: { buttons: ['ul', 'ol', 'hline', 'table'] },
+	}
+}
+
 export default class TextEditor extends Component {
 	componentDidMount() {
-		this.editor = AlloyEditor.editable(this.container)
+		this.editor = AlloyEditor.editable(this.container, ALLOY_CONFIG)
 
 		this.componentWillReceiveProps(this.props)
 	}
