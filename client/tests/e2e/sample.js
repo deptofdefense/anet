edd188095c05d6a5329d1de500233ab93bb9a3c9
Nick Heiner <nth23@cornell.edu>
2017-03-20 16:12:58 -0400

    Use firefox instead

--- client/tests/e2e/sample.js
+++ client/tests/e2e/sample.js
@@ -1,20 +1,39 @@
 let test = require('ava'),
     webdriver = require('selenium-webdriver'),
+    geckodriver = require('geckodriver'),
     By = webdriver.By
 
 webdriver.promise.USE_PROMISE_MANAGER = false
 
+test.before(() => geckodriver.start())
+
 test.beforeEach(t => {
     t.context.driver = new webdriver.Builder()
-        .forBrowser('chrome')
+        .forBrowser('firefox')
         .build()
 
-    t.context.get = pathname => t.context.driver.get(`http://localhost:3000${pathname}`)
+    t.context.get = async pathname => {
+        await t.context.driver.get(`http://localhost:3000${pathname}`)
+        // await new Promise(resolve => setTimeout(resolve, 5000))
+        // await t.context.driver.wait(webdriver.until.alertIsPresent())
+        // try {
+        //     let alert = await t.context.driver.switchTo().alert()
+        //     await alert.authenticateAs('erin', 'erin')
+        // } catch (e) {
+        //     if (e.code !== 27) {
+        //         throw e
+        //     }
+        // }
+    }
 })
 
 test.afterEach.always(async t => {
-    await t.context.driver.quit()
+    if (t.context.driver) {
+        await t.context.driver.quit()
+    }
 })
+
+test.after.always(() => geckodriver.stop())
 
 test('My ANET snapshot', async t => {
     t.plan(1)
