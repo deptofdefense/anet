f1e9c961c06b098b02db2669ecf43941c89dfa5b
Nick Heiner <nth23@cornell.edu>
2017-03-20 15:51:15 -0400

    always quit

--- client/tests/e2e/sample.js
+++ client/tests/e2e/sample.js
@@ -2,6 +2,8 @@ let test = require('ava'),
     webdriver = require('selenium-webdriver'),
     By = webdriver.By
 
+webdriver.promise.USE_PROMISE_MANAGER = false
+
 test.beforeEach(t => {
     t.context.driver = new webdriver.Builder()
         .forBrowser('chrome')
@@ -10,13 +12,13 @@ test.beforeEach(t => {
     t.context.get = pathname => t.context.driver.get(`http://localhost:3000${pathname}`)
 })
 
-test.afterEach(async t => {
+test.afterEach.always(async t => {
     await t.context.driver.quit()
 })
 
 test('My ANET snapshot', async t => {
     t.plan(1)
     await t.context.get('/')
-    let reportsPendingMyApproval = await t.context.driver.findElement(By.css('.home-tile:first h1')).getText()
-    t.equal(reportsPendingMyApproval, '0')
+    let reportsPendingMyApproval = await t.context.driver.findElement(By.css('.home-tile:first-child h1')).getText()
+    t.is(reportsPendingMyApproval, '0')
 })
