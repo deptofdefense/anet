9989eaeaee75f90bb36aeffe3d93940b7e723971
Nick Heiner <nth23@cornell.edu>
2017-03-20 11:52:35 -0400

    Remove Chrome version -- no need to peg it

--- client/tests/intern.js
+++ client/tests/intern.js
@@ -21,7 +21,7 @@ define({
 //		{ browserName: 'internet explorer', version: '10', platform: 'WIN8' },
 //		{ browserName: 'internet explorer', version: '9', platform: 'WINDOWS' },
 //		{ browserName: 'firefox', version: '37', platform: [ 'WINDOWS', 'MAC' ] },
-		{ browserName: 'chrome', version: '39', platform: [ 'MAC' ] },
+		{ browserName: 'chrome', platform: [ 'MAC' ] },
 //		{ browserName: 'safari', version: '8', platform: 'MAC' }
 	],
 
