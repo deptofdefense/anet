33c6b6fad37d6f641b52c60db37b263c22ea83c3
Nick Heiner <nth23@cornell.edu>
2017-03-20 15:34:42 -0400

    basic test exists, but it needs to get past auth

--- client/tests/e2e/sample.js
+++ client/tests/e2e/sample.js
@@ -1,24 +1,22 @@
 let test = require('ava'),
     webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until
+    By = webdriver.By
 
 test.beforeEach(t => {
     t.context.driver = new webdriver.Builder()
         .forBrowser('chrome')
         .build()
 
-    t.context.get = pathname => t.context.driver.get(`http://localhost:3000/${pathname}`)
+    t.context.get = pathname => t.context.driver.get(`http://localhost:3000${pathname}`)
 })
 
 test.afterEach(async t => {
     await t.context.driver.quit()
 })
 
-test('First test', async t => {
-    t.context.get('/')
-    t.context.driver.findElement(By.name('q')).sendKeys('webdriver')
-    t.context.driver.findElement(By.name('btnG')).click()
-    t.context.driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+test('My ANET snapshot', async t => {
+    t.plan(1)
+    await t.context.get('/')
+    let reportsPendingMyApproval = await t.context.driver.findElement(By.css('.home-tile:first h1')).getText()
+    t.equal(reportsPendingMyApproval, '0')
 })
-
