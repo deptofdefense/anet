f5f707f02d5eff7b4565f0051959f34562d0cf83
Nick Heiner <nth23@cornell.edu>
2017-03-20 14:53:09 -0400

    scratch

--- client/tests/e2e/sample.js
+++ client/tests/e2e/sample.js
@@ -7,12 +7,18 @@ test.beforeEach(t => {
     t.context.driver = new webdriver.Builder()
         .forBrowser('chrome')
         .build()
+
+    t.context.get = pathname => t.context.driver.get(`http://localhost:3000/${pathname}`)
 })
 
-test('Google test', async t => {
-    t.context.driver.get('http://www.google.com/ncr')
+test.afterEach(async t => {
+    await t.context.driver.quit()
+})
+
+test('First test', async t => {
+    t.context.get('/')
     t.context.driver.findElement(By.name('q')).sendKeys('webdriver')
     t.context.driver.findElement(By.name('btnG')).click()
     t.context.driver.wait(until.titleIs('webdriver - Google Search'), 1000)
-    await t.context.driver.quit()
 })
+
