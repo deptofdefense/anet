5398b40d3c011d79487f734b58cb9f7b90460d84
Hunter Pitelka <hunter@dds.mil>
2017-03-25 12:24:41 +0430

    Keep identity information when importing from backup.

--- docs/anet_import.ps1
+++ docs/anet_import.ps1
@@ -4,18 +4,18 @@ $DB_PASSWORD = ""
 $indir = "backups"
 
 
-bcp dbo.adminSettings IN $indir/adminSettings.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.approvalActions IN $indir/approvalActions.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.approvalSteps IN $indir/approvalSteps.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.approvers IN $indir/approvers.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.locations IN $indir/locations.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.organizations IN $indir/organizations.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.people IN $indir/people.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.peoplePositions IN $indir/peoplePositions.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.poams IN $indir/poams.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.positionRelationships IN $indir/positionRelationships.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.positions IN $indir/positions.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.reportPeople IN $indir/reportPeople.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.reportPoams IN $indir/reportPoams.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.reports IN $indir/reports.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
-bcp dbo.savedSearches IN $indir/savedSearches.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD"
+bcp dbo.adminSettings IN $indir/adminSettings.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.approvalActions IN $indir/approvalActions.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.approvalSteps IN $indir/approvalSteps.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.approvers IN $indir/approvers.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.locations IN $indir/locations.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.organizations IN $indir/organizations.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.people IN $indir/people.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.peoplePositions IN $indir/peoplePositions.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.poams IN $indir/poams.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.positionRelationships IN $indir/positionRelationships.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.positions IN $indir/positions.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -q -E
+bcp dbo.reportPeople IN $indir/reportPeople.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.reportPoams IN $indir/reportPoams.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.reports IN $indir/reports.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
+bcp dbo.savedSearches IN $indir/savedSearches.bak -N  -d $DB_NAME -U $DB_USERNAME -P "$DB_PASSWORD" -E
