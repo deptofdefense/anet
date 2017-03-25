75311faacace2e428292eb13117bc8431a7e6d84
Hunter Pitelka <hunter@dds.mil>
2017-03-25 18:15:05 +0430

    More descriptive name on Approval Step Edit.

--- client/src/pages/organizations/Form.js
+++ client/src/pages/organizations/Form.js
@@ -88,6 +88,7 @@ export default class OrganizationForm extends ValidatableFormWrapper {
 			</Button>
 
 			<Form.Field id="name"
+				label="Step name"
 				value={step.name}
 				onChange={(event) => this.setStepName(index, event)} />
 
