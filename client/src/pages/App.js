5afd25f7b16b00d0f58fcdf4187b5fcbdc61e45a
Nick Heiner <nth23@cornell.edu>
2017-03-28 14:40:17 -0400

    Fixing up search results and other report list views for print.
    Addresses #642.

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -107,7 +107,7 @@ export default class App extends Page {
 					{pageProps.useNavigation === false
 						? <Row><Col xs={12}>{this.props.children}</Col></Row>
 						: <Row>
-							<Col sm={3}>
+							<Col sm={3} className="hide-for-print">
 								<Nav />
 							</Col>
 							<Col sm={9} className="primary-content">
