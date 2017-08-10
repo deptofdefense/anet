c417f5c2fe1b3b136f39b6b1e9f35d940985879f
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-08-10 16:22:45 +0200

    Add NoPositionBanner to TopBar component

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 
+import NoPositionBanner from 'components/NoPositionBanner'
 import GeneralBanner from 'components/GeneralBanner'
 import SecurityBanner from 'components/SecurityBanner'
 import Header from 'components/Header'
@@ -29,14 +30,16 @@ export default class TopBar extends Component {
     updateBodyPaddingTop() {
         let topbarPaddingHeight = document.getElementById('topbar').offsetHeight + 20
         if (this.state.bodyPaddingTop !== topbarPaddingHeight){
+            document.body.style.paddingTop=`${topbarPaddingHeight}px` 
             this.setState({ bodyPaddingTop: topbarPaddingHeight })
-            document.body.style.paddingTop=`${this.state.bodyPaddingTop}px` 
         }
     }
 
 	render() {
+        let currentUser = this.state.currentUser
 		return (
 			<div id="topbar" className="navbar navbar-fixed-top">
+                {currentUser && currentUser.position && currentUser.position.id === 0 && !currentUser.isNewUser() && <NoPositionBanner />}
 				<GeneralBanner banner={this.props.banner} />
                 <SecurityBanner location={this.props.location} />
 			    <Header minimalHeader={this.props.minimalHeader} />
