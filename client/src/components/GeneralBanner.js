01b1c72bb7a4a382ab2dcce83a337e66dffc7f4c
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-08-10 13:46:25 +0200

    Adds the title announcement  to general banner banner

--- client/src/components/GeneralBanner.js
+++ client/src/components/GeneralBanner.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const GeneralBanner = (props) => {
     const bannerClassName = `general-banner alert ${props.banner.statusColor}`
+    
     return (
         <div className={bannerClassName}>
-            <div className="messsage">{props.banner.message}</div>
+            <div className="messsage"><strong>Announcement:</strong> {props.banner.message}</div>
         </div>
     )
 }
