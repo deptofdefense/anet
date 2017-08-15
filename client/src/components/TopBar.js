bbe8234392de6d9006027a3fe4f4f9aa97f19654
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-08-15 16:36:36 +0200

    Determine banner settings in TopBar instead of App.js

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,11 +1,20 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 
 import NoPositionBanner from 'components/NoPositionBanner'
 import GeneralBanner from 'components/GeneralBanner'
 import SecurityBanner from 'components/SecurityBanner'
 import Header from 'components/Header'
 
+// const GENERAL_BANNER_COLOR = 'alert-info'
+const GENERAL_BANNER_TEXT = 'GENERAL_BANNER_TEXT'
+// const GENERAL_BANNER_TITLE = 'Announcement'
+
+
 export default class TopBar extends Component {
+    static contextTypes = {
+		app: PropTypes.object.isRequired,
+    }
+
     constructor(props) {
         super(props)
         this.state = { 
@@ -37,10 +46,19 @@ export default class TopBar extends Component {
 
 	render() {
         let currentUser = this.state.currentUser
+        let app = this.context.app
+        let {settings} = app.state
+        
+        let banner = {
+			message: settings[GENERAL_BANNER_TEXT],
+			title: 'Announcement',
+			color: 'alert-info'
+		} || {}
+
 		return (
 			<div id="topbar" className="navbar navbar-fixed-top">
                 {currentUser && currentUser.position && currentUser.position.id === 0 && !currentUser.isNewUser() && <NoPositionBanner />}
-				<GeneralBanner banner={this.props.banner} />
+				{banner.message && <GeneralBanner banner={banner} />}
                 <SecurityBanner location={this.props.location} />
 			    <Header minimalHeader={this.props.minimalHeader} />
             </div>
