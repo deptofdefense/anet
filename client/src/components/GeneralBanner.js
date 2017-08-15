b9d1bbb45ffcbd6b7eea08db6574c0d09ab8f359
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-08-15 16:35:10 +0200

    Refactor GeneralBanner component to use prop values

--- client/src/components/GeneralBanner.js
+++ client/src/components/GeneralBanner.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react'
 
 export default class GeneralBanner extends Component {
     render() {
-        const bannerClassName = `general-banner alert ${this.props.banner.statusColor}`
+        const bannerClassName = `general-banner alert ${this.props.banner.color}`
+
         return (
             <div className={bannerClassName}>
-                <div className="messsage"><strong>Announcement:</strong> {this.props.banner.message}</div>
+                <div className="messsage"><strong>{this.props.banner.title}:</strong> {this.props.banner.message}</div>
             </div>
         )
     }
