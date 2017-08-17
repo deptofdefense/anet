eb9b0e0982bd6432618e3b98956c626a831f27e8
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-08-17 14:07:21 +0200

    Refactor GeneralBanner component to a function

--- client/src/components/GeneralBanner.js
+++ client/src/components/GeneralBanner.js
@@ -1,23 +1,25 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-export default class GeneralBanner extends Component {
-
-    bannerClassName(level){
-        var output = 'general-banner alert'
-        switch (level) {
-            case 'notice':  return output += ' alert-info'
-            case 'success': return output += ' alert-success'
-            case 'error':   return output += ' alert-danger'
-            case 'alert':   return output += ' alert-warning'
-            default:        return output
-        }
-    }
-
-    render() {
-        return (
-            <div className={this.bannerClassName(this.props.banner.level)}>
-                <div className="messsage"><strong>{this.props.banner.title}:</strong> {this.props.banner.message}</div>
-            </div>
-        )
+function bannerClassName(level) {
+    var output = 'general-banner alert'
+    switch (level) {
+        case 'notice':  return output += ' alert-info'
+        case 'success': return output += ' alert-success'
+        case 'error':   return output += ' alert-danger'
+        case 'alert':   return output += ' alert-warning'
+        default:        return output
     }
 }
+
+const GeneralBanner = (props) => {
+    const banner = props.banner
+    const showBanner = props.showBanner
+    if (showBanner && banner.message){
+        return (
+            <div className={bannerClassName(banner.level)}>
+                <div className="messsage"><strong>{banner.title}:</strong> {banner.message}</div>
+            </div>
+        )
+    } else { return null }
+}
+export default GeneralBanner
