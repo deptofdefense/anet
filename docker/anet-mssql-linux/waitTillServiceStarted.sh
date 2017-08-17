7a4a7f280dfba47fe43cf309f1eecc9343ce300c
Gertjan van Oosten <gertjan@west.nl>
2017-08-17 09:24:05 +0200

    Minor improvement to MS SQL startup progress indicator

--- docker/anet-mssql-linux/waitTillServiceStarted.sh
+++ docker/anet-mssql-linux/waitTillServiceStarted.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo waiting for MS SQL to start
+echo -n Waiting for MS SQL to start
 for i in {60..0}; do
   if /opt/mssql-tools/bin/sqlcmd -d "$DB_NAME" -U "$DB_USER" -P "$DB_USER_PASSWORD" -Q 'SELECT 1;' &> /dev/null; then
+    echo done
     break
   fi
-  echo .
+  echo -n .
   sleep 1
 done
