554719edab2b9f58ed21e7d2de0f14a9e6c77ab1
Mara Dragan <mara@west.nl>
2017-09-05 11:55:13 +0200

    NCI-Agency/anet#127: Add tooltip on click on tags

--- client/src/components/Tag.js
+++ client/src/components/Tag.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import {Label} from 'react-bootstrap'
+import {Label, OverlayTrigger, Tooltip} from 'react-bootstrap'
 
-const Tag = ({tag}) => (
-  <Label bsStyle="info">{tag.name}</Label>
-)
+const Tag = ({tag}) => {
+  var tagDisplay
+  tagDisplay = <Label bsStyle="info">{tag.name}</Label>
+  if (tag.description) {
+    tagDisplay = <OverlayTrigger trigger="click" placement="bottom" overlay={<Tooltip id="tooltip">{tag.description}</Tooltip>}>{tagDisplay}</OverlayTrigger>
+  }
+  return tagDisplay
+}
     
 export default Tag
