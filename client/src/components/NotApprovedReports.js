f2220a8abca3c6dafa2821609739fb900b50d542
Mara Dragan <mara@west.nl>
2017-09-14 17:39:13 +0200

    NCI-Agency/anet#155: Update data retrieval for not approved reports chart

--- client/src/components/NotApprovedReports.js
+++ client/src/components/NotApprovedReports.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
 import moment from 'moment'
-
 import API from 'api'
 import dict from 'dictionary'
+
 import BarChart from 'components/BarChart'
+import ReportCollection from 'components/ReportCollection'
 
 
 export default class NotApprovedReports extends Component {
@@ -16,39 +17,39 @@ export default class NotApprovedReports extends Component {
     }
   }
 
-  get rollupStart() { return moment(this.state.date).subtract(100, 'days').startOf('day').hour(19) } //7pm yesterday
-  get rollupEnd() { return moment(this.state.date).endOf('day').hour(18) } // 6:59:59pm today.
-
   render() {
-    return <BarChart data={this.state.graphData} size={[500,500]} xProp='org.shortName' yProp='released' />
+    // FIXME: xProp should be advisorOrg.id and the xLabel should be advisorOrg.shortName
+    return <BarChart data={this.state.graphData} size={[500,500]} xProp='advisorOrg.shortName' yProp='notApproved' />
   }
 
   fetchData() {
-    let graphQueryUrl = `/api/reports/rollupGraph?startDate=${this.rollupStart.valueOf()}&endDate=${this.rollupEnd.valueOf()}`
-    if (this.state.focusedOrg) {
-      if (this.state.orgType === 'PRINCIPAL_ORG') {
-        graphQueryUrl += `&principalOrganizationId=${this.state.focusedOrg.id}`
-      } else {
-        graphQueryUrl += `&advisorOrganizationId=${this.state.focusedOrg.id}`
-      }
-    } else if (this.state.orgType) {
-      graphQueryUrl += `&orgType=${this.state.orgType}`
+    const insightQuery = {
+      state: ['PENDING_APPROVAL'],
+      updatedAtEnd: -1*24*3600,
+      pageNum: this.state.reportsPageNum,
     }
 
-    let graphQuery = API.fetch(graphQueryUrl)
+    let reportQuery = API.query(/* GraphQL */`
+        reportList(f:search, query:$insightQuery) {
+          pageNum, pageSize, totalCount, list {
+            ${ReportCollection.GQL_REPORT_FIELDS}
+          }
+        }
+      `, {insightQuery}, '($insightQuery: ReportSearchQuery)')
+
     let pinned_ORGs = dict.lookup('pinned_ORGs')
 
-    Promise.all([graphQuery]).then(values => {
+    Promise.all([reportQuery]).then(values => {
       this.setState({
-        graphData: values[0]
-          .map(d => {d.org = d.org || {id: -1, shortName: "Other"}; return d})
+        graphData: values[0].reportList.list
+          .map(d => {d.notApproved = values[0].reportList.list.filter(item => item.advisorOrg.shortName === d.advisorOrg.shortName).length; return d})
           .sort((a, b) => {
-            let a_index = pinned_ORGs.indexOf(a.org.shortName)
-            let b_index = pinned_ORGs.indexOf(b.org.shortName)
-            if (a_index<0)
-              return (b_index<0) ?  a.org.shortName.localeCompare(b.org.shortName) : 1
+            let a_index = pinned_ORGs.indexOf(a.advisorOrg.shortName)
+            let b_index = pinned_ORGs.indexOf(b.advisorOrg.shortName)
+            if (a_index < 0)
+              return (b_index < 0) ?  a.advisorOrg.shortName.localeCompare(b.advisorOrg.shortName) : 1
             else
-              return (b_index<0) ? -1 : a_index-b_index
+              return (b_index < 0) ? -1 : a_index-b_index
           })
       })
     })
