1e86ab4afc619ff64ee96dbcdbc79c5b82eaaa23
Gertjan van Oosten <gertjan@west.nl>
2017-09-26 14:29:09 +0200

    NCI-Agency/anet#126: Add specific styling for report tags
    Add styles for display and edit modes.

--- client/src/components/Tag.js
+++ client/src/components/Tag.js
@@ -3,11 +3,11 @@ import {Label, OverlayTrigger, Tooltip} from 'react-bootstrap'
 
 const Tag = ({tag}) => {
   var tagDisplay
-  tagDisplay = <Label bsStyle="info">{tag.name}</Label>
+  tagDisplay = <Label bsStyle="info" className="reportTag">{tag.name}</Label>
   if (tag.description) {
     tagDisplay = <OverlayTrigger trigger="click" placement="bottom" overlay={<Tooltip id="tooltip">{tag.description}</Tooltip>}>{tagDisplay}</OverlayTrigger>
   }
   return tagDisplay
 }
-    
+
 export default Tag
--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -4,8 +4,6 @@ import {Alert, Table, Button, Col, Modal, Checkbox} from 'react-bootstrap'
 import autobind from 'autobind-decorator'
 import moment from 'moment'
 import utils from 'utils'
-import { WithContext as ReactTags } from 'react-tag-input'
-import 'components/reactTags.css'
 
 import History from 'components/History'
 import Fieldset from 'components/Fieldset'
@@ -13,6 +11,7 @@ import Breadcrumbs from 'components/Breadcrumbs'
 import Form from 'components/Form'
 import Messages from 'components/Messages'
 import LinkTo from 'components/LinkTo'
+import Tag from 'components/Tag'
 
 import API from 'api'
 import dict from 'dictionary'
@@ -201,9 +200,7 @@ export default class ReportShow extends Page {
 							</Form.Field>
 						}
 						<Form.Field id="tags" label="Tags">
-							<ReactTags tags={report.tags}
-								labelField={'name'}
-								readOnly={true} />
+							{report.tags && report.tags.map((tag,i) => <Tag key={tag.id} tag={tag} />)}
 						</Form.Field>
 						<Form.Field id="author" label="Report author">
 							<LinkTo person={report.author} />
