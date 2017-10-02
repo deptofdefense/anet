a2f85a0408b08325497823129d668d89a44015e8
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-10-02 12:11:51 +0200

    Fix merge conflict on insights show page

--- client/src/pages/insights/Show.js
+++ client/src/pages/insights/Show.js
@@ -88,15 +88,6 @@ export default class InsightsShow extends Page {
               <CancelledReports date={this.state.cancelledDate.clone().startOf('day')} />
           </Fieldset>
         }
-        <Fieldset id="cancelled-reports" data-jumptarget title={
-          <span>
-            Cancelled Reports - {this.cancelledDateLongStr}
-            <CalendarButton onChange={this.changeCancelledDate} value={this.state.cancelledDate.toISOString()} style={calendarButtonCss} />
-          </span>
-          }>
-            <p className="help-text">Number of reports cancelled since {this.cancelledDateLongStr}</p>
-            <CancelledReports date={this.state.cancelledDate} />
-        </Fieldset>
 
         <Fieldset id="advisor-reports" data-jumptarget title={
           <span>
