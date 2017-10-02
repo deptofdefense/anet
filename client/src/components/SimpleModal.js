fcb56cb2079f75b235ae0ed88ee6c60142153a9f
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-10-02 11:27:03 +0200

    NCI-Agency/anet#159 Adds SimpleModal component

---
+++ client/src/components/SimpleModal.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Button, Modal } from 'react-bootstrap'
+
+class SimpleModal extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            showModal: false
+        }
+        this.close = this.close.bind(this)
+        this.open = this.open.bind(this)
+    }
+
+    close() {
+        this.setState({ showModal: false })
+    }
+
+    open() {
+        this.setState({ showModal: true })
+    }
+
+    render() {
+          return (
+            <div>
+              <a onClick={ this.open } >
+                { this.props.title }
+              </a>
+      
+              <Modal bsSize={ this.props.size } show={ this.state.showModal } onHide={ this.close }>
+                <Modal.Header closeButton>
+                  <Modal.Title>{ this.props.title }</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    { this.props.children }
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button onClick={ this.close }>Close</Button>
+                </Modal.Footer>
+              </Modal>
+            </div>
+          )
+    }
+}
+
+export default SimpleModal
