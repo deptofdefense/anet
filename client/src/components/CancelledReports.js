dfc4a2c09768627b73e088f0b71c36f04eb36b9f
Mara Dragan <mara@west.nl>
2017-10-03 17:07:05 +0200

    NCI-Agency/anet#157: Handle click on bar of insight cancelled reports by org

--- client/src/components/CancelledReports.js
+++ client/src/components/CancelledReports.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react'
 import API from 'api'
 import dict from 'dictionary'
+import autobind from 'autobind-decorator'
+import {Button} from 'react-bootstrap'
 
 import BarChart from 'components/BarChart'
+import Fieldset from 'components/Fieldset'
 import ReportCollection from 'components/ReportCollection'
 
 
+const d3 = require('d3')
 const colors = {
-    barColor: '#F5CA8D',
-    selectedBarColor: '#EC971F'
-  }
+  barColor: '#F5CA8D',
+  selectedBarColor: '#EC971F'
+}
+const chartByOrgId = 'cancelled_reports_by_org'
+const chartByReasonId = 'cancelled_reports_by_reason'
 
 
 /*
@@ -28,51 +34,80 @@ export default class CancelledReports extends Component {
       date: props.date,
       graphDataByOrg: [],
       graphDataByReason: [],
+      focusedOrg: ''
     }
   }
 
   render() {
+    let chartByOrg = ''
+    let chartByReason = ''
     if (this.state.graphDataByOrg.length) {
-      return <div>
-          <BarChart data={this.state.graphDataByOrg} xProp='advisorOrg.id' yProp='cancelledByOrg' xLabel='advisorOrg.shortName' barColor={colors.barColor} />
-          <BarChart data={this.state.graphDataByReason} xProp='reason' yProp='cancelledByReason' barColor={colors.barColor} />
-        </div>
+      chartByOrg = <BarChart
+        chartId={chartByOrgId}
+        data={this.state.graphDataByOrg}
+        xProp='advisorOrg.id'
+        yProp='cancelledByOrg'
+        xLabel='advisorOrg.shortName'
+        onBarClick={this.goToOrg}
+        barColor={colors.barColor}
+      />
+      chartByReason = <BarChart
+        chartId={chartByReasonId}
+        data={this.state.graphDataByReason}
+        xProp='reason'
+        yProp='cancelledByReason'
+        barColor={colors.barColor}
+      />
     }
-    else {
-      return <div>No such reports.</div>
-    }
+    return (
+      <div>
+        {chartByOrg}
+        {chartByReason}
+        <Fieldset
+            title={`Cancelled reports ${this.state.focusedOrg ? `for ${this.state.focusedOrg.shortName}` : ''}`}
+            id='cancelled-reports-details'
+            action={!this.state.focusedOrg
+              ? '' : <Button onClick={() => this.goToOrg()}>All organizations</Button>
+            }
+          >
+          <ReportCollection paginatedReports={this.state.reports} goToPage={this.goToReportsPage} />
+        </Fieldset>
+      </div>
+    )
   }
 
   fetchData() {
-    const insightQuery = {
+    let pinned_ORGs = dict.lookup('pinned_ORGs')
+    const commonQueryParams = {
       state: ['CANCELLED'],
       releasedAtStart: this.state.date.valueOf(),
-      pageSize: 0,  // retrieve all the filtered reports
     }
-
-    let reportQuery = API.query(/* GraphQL */`
-        reportList(f:search, query:$insightQuery) {
+    const chartQueryParams = {}
+    Object.assign(chartQueryParams, commonQueryParams)
+    Object.assign(chartQueryParams, {
+      pageSize: 0,  // retrieve all the filtered reports
+    })
+    // Query used by the chart
+    let chartQuery = API.query(/* GraphQL */`
+        reportList(f:search, query:$chartQueryParams) {
           totalCount, list {
             ${ReportCollection.GQL_REPORT_FIELDS}
           }
         }
-      `, {insightQuery}, '($insightQuery: ReportSearchQuery)')
-
-    let pinned_ORGs = dict.lookup('pinned_ORGs')
-
-    Promise.all([reportQuery]).then(values => {
+      `, {chartQueryParams}, '($chartQueryParams: ReportSearchQuery)')
+    Promise.all([chartQuery]).then(values => {
       this.setState({
         graphDataByOrg: values[0].reportList.list
-          .filter((item, index, d) => d.findIndex(t => {return t.advisorOrg.id === item.advisorOrg.id }) === index)
-          .map(d => {d.cancelledByOrg = values[0].reportList.list.filter(item => item.advisorOrg.id === d.advisorOrg.id).length; return d})
-          .sort((a, b) => {
-            let a_index = pinned_ORGs.indexOf(a.advisorOrg.shortName)
-            let b_index = pinned_ORGs.indexOf(b.advisorOrg.shortName)
-            if (a_index < 0)
-              return (b_index < 0) ?  a.advisorOrg.shortName.localeCompare(b.advisorOrg.shortName) : 1
-            else
-              return (b_index < 0) ? -1 : a_index-b_index
-          }),
+        .filter((item, index, d) => d.findIndex(t => {return t.advisorOrg.id === item.advisorOrg.id }) === index)
+        .map(d => {d.cancelledByOrg = values[0].reportList.list.filter(item => item.advisorOrg.id === d.advisorOrg.id).length; return d})
+        .sort((a, b) => {
+          let a_index = pinned_ORGs.indexOf(a.advisorOrg.shortName)
+          let b_index = pinned_ORGs.indexOf(b.advisorOrg.shortName)
+          if (a_index < 0)
+            return (b_index < 0) ?  a.advisorOrg.shortName.localeCompare(b.advisorOrg.shortName) : 1
+          else
+            return (b_index < 0) ? -1 : a_index-b_index
+        }),
         graphDataByReason: values[0].reportList.list
           .filter((item, index, d) => d.findIndex(t => {return t.cancelledReason === item.cancelledReason }) === index)
           .map(d => {d.cancelledByReason = values[0].reportList.list.filter(item => item.cancelledReason === d.cancelledReason).length; return d})
@@ -82,8 +117,55 @@ export default class CancelledReports extends Component {
         })
       })
     })
-  }  
-  
+    this.fetchOrgData()
+  }
+
+  fetchOrgData() {
+    const commonQueryParams = {
+      state: ['CANCELLED'],
+      releasedAtStart: this.state.date.valueOf(),
+    }
+    const reportsQueryParams = {}
+    Object.assign(reportsQueryParams, commonQueryParams)
+    Object.assign(reportsQueryParams, {pageNum: this.state.reportsPageNum})
+    if (this.state.focusedOrg) {
+      Object.assign(reportsQueryParams, {advisorOrgId: this.state.focusedOrg.id})
+    }
+    // Query used by the reports collection
+    let reportsQuery = API.query(/* GraphQL */`
+        reportList(f:search, query:$reportsQueryParams) {
+          pageNum, pageSize, totalCount, list {
+            ${ReportCollection.GQL_REPORT_FIELDS}
+          }
+        }
+      `, {reportsQueryParams}, '($reportsQueryParams: ReportSearchQuery)')
+    Promise.all([reportsQuery]).then(values => {
+      this.setState({
+        reports: values[0].reportList
+      })
+    })
+  }
+
+  @autobind
+  goToReportsPage(newPage) {
+    this.setState({reportsPageNum: newPage}, () => this.fetchOrgData())
+  }
+
+  resetChartSelection(chartId) {
+    d3.selectAll('#' + chartId + ' rect').attr('fill', colors.barColor)
+  }
+
+  @autobind
+  goToOrg(item) {
+    this.setState({reportsPageNum: 0, focusedOrg: (item ? item.advisorOrg : '')}, () => this.fetchOrgData())
+    // remove highlighting of the bars
+    this.resetChartSelection(chartByOrgId)
+    if (item) {
+      // highlight the bar corresponding to the selected organisation
+      d3.select('#' + chartByOrgId + ' #bar_' + item.advisorOrg.id).attr('fill', colors.selectedBarColor)
+    }
+  }
+
   componentWillReceiveProps(nextProps, nextContext) {
     if (nextProps !== this.props) {
       this.setState({date: nextProps.date})
