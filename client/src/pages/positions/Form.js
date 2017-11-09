deptofdefense/anet

commit 11a734cf064e4bb978c456b381c7648077aa4c26

NCI-Agency/anet#195: Extract constants for position type

client/src/pages/positions/Form.js
@@ -39,7 +39,7 @@ export default class PositionForm extends ValidatableFormWrapper {
 			orgSearchQuery.type = 'PRINCIPAL_ORG'
 		} else {
 			orgSearchQuery.type = 'ADVISOR_ORG'
-			if (currentUser && currentUser.position && currentUser.position.type === 'SUPER_USER') {
+			if (currentUser && currentUser.position && currentUser.position.type === Position.TYPE.SUPER_USER) {
 				orgSearchQuery.parentOrgId = currentUser.position.organization.id
 				orgSearchQuery.parentOrgRecursively = true
 			}
@@ -63,8 +63,8 @@ export default class PositionForm extends ValidatableFormWrapper {
 				<Fieldset title={edit ? `Edit Position ${position.name}` : "Create a new Position"}>
 					<Form.Field id="type" disabled={this.props.edit}>
 						<ButtonToggleGroup>
-							<Button id="typeAdvisorButton" value="ADVISOR">{dict.lookup('ADVISOR_POSITION_NAME')}</Button>
-							<Button id="typePrincipalButton" value="PRINCIPAL">{dict.lookup('PRINCIPAL_POSITION_NAME')}</Button>
+							<Button id="typeAdvisorButton" value="{Position.TYPE.ADVISOR}">{dict.lookup('ADVISOR_POSITION_NAME')}</Button>
+							<Button id="typePrincipalButton" value="{Position.TYPE.PRINCIPAL}">{dict.lookup('PRINCIPAL_POSITION_NAME')}</Button>
 						</ButtonToggleGroup>
 					</Form.Field>
 
@@ -91,18 +91,18 @@ export default class PositionForm extends ValidatableFormWrapper {
 					</Form.Field>
 
 					<Form.Field id="code"
-						label={position.type === 'PRINCIPAL' ? dict.lookup('PRINCIPAL_POSITION_CODE_NAME') : dict.lookup('ADVISOR_POSITION_CODE_NAME')}
+						label={position.type === Position.TYPE.PRINCIPAL ? dict.lookup('PRINCIPAL_POSITION_CODE_NAME') : dict.lookup('ADVISOR_POSITION_CODE_NAME')}
 						placeholder="Postion ID or Number" />
 
 					<RequiredField id="name" label="Position Name" placeholder="Name/Description of Position"/>
 
-					{position.type !== 'PRINCIPAL' &&
+					{position.type !== Position.TYPE.PRINCIPAL &&
 						<Form.Field id="permissions">
 							<ButtonToggleGroup>
-								<Button id="permsAdvisorButton" value="ADVISOR">{dict.lookup('ADVISOR_POSITION_TYPE_TITLE')}</Button>
-								<Button id="permsSuperUserButton" value="SUPER_USER">{dict.lookup('SUPER_USER_POSITION_TYPE_TITLE')}</Button>
+								<Button id="permsAdvisorButton" value="{Position.TYPE.ADVISOR}">{dict.lookup('ADVISOR_POSITION_TYPE_TITLE')}</Button>
+								<Button id="permsSuperUserButton" value="{Position.TYPE.SUPER_USER}">{dict.lookup('SUPER_USER_POSITION_TYPE_TITLE')}</Button>
 								{currentUser && currentUser.isAdmin() &&
-									<Button id="permsAdminButton" value="ADMINISTRATOR">{dict.lookup('ADMINISTRATOR_POSITION_TYPE_TITLE')}</Button>
+									<Button id="permsAdminButton" value="{Position.TYPE.ADMINISTRATOR}">{dict.lookup('ADMINISTRATOR_POSITION_TYPE_TITLE')}</Button>
 								}
 							</ButtonToggleGroup>
 						</Form.Field>
@@ -130,8 +130,8 @@ export default class PositionForm extends ValidatableFormWrapper {
 		let {position, edit} = this.props
 		position = Object.assign({}, position)
 
-		if (position.type !== 'PRINCIPAL') {
-			position.type = position.permissions || 'ADVISOR'
+		if (position.type !== Position.TYPE.PRINCIPAL) {
+			position.type = position.permissions || Position.TYPE.ADVISOR
 		}
 		delete position.permissions
 		position.organization = {id: position.organization.id}

client/src/pages/organizations/Form.js
@@ -105,7 +105,7 @@ export default class OrganizationForm extends ValidatableFormWrapper {
 						pos.code && components.push(pos.code)
 						return <span>{components.join(' - ')}</span>
 					}}
-					queryParams={{type: ['ADVISOR', 'SUPER_USER', 'ADMINISTRATOR'], matchPersonName: true}}
+					queryParams={{type: [Position.TYPE.ADVISOR, Position.TYPE.SUPER_USER, Position.TYPE.ADMINISTRATOR], matchPersonName: true}}
 					onChange={this.addApprover.bind(this, index)}
 					clearOnSelect={true} />
 

client/src/pages/poams/Form.js
@@ -13,7 +13,7 @@ import ButtonToggleGroup from 'components/ButtonToggleGroup'
 
 import dict from 'dictionary'
 import API from 'api'
-import {Poam} from 'models'
+import {Poam, Position} from 'models'
 
 export default class PoamForm extends ValidatableFormWrapper {
 	static propTypes = {
@@ -32,7 +32,7 @@ export default class PoamForm extends ValidatableFormWrapper {
 
 		let orgSearchQuery = {}
 		orgSearchQuery.type = 'ADVISOR_ORG'
-		if (currentUser && currentUser.position && currentUser.position.type === 'SUPER_USER') {
+		if (currentUser && currentUser.position && currentUser.position.type === Position.TYPE.SUPER_USER) {
 			orgSearchQuery.parentOrgId = currentUser.position.organization.id
 			orgSearchQuery.parentOrgRecursively = true
 		}

client/src/components/AdvancedSearch.js
@@ -13,7 +13,7 @@ import AutocompleteFilter from 'components/advancedSearch/AutocompleteFilter'
 import OrganizationFilter from 'components/advancedSearch/OrganizationFilter'
 import SelectSearchFilter from 'components/advancedSearch/SelectSearchFilter'
 
-import {Person, Poam} from 'models'
+import {Person, Poam, Position} from 'models'
 
 import REMOVE_ICON from 'resources/delete.png'
 
@@ -131,7 +131,7 @@ export default class AdvancedSearch extends Component {
 			filters: {
 				"Position type": <SelectSearchFilter
 					queryKey="type"
-					values={["ADVISOR", "PRINCIPAL"]}
+					values={[Position.TYPE.ADVISOR, Position.TYPE.PRINCIPAL]}
 					labels={[dict.lookup('ADVISOR_POSITION_NAME'), dict.lookup('PRINCIPAL_POSITION_NAME')]}
 				/>,
 				Organization: <OrganizationFilter
@@ -335,9 +335,9 @@ class SearchFilter extends Component {
 			let organizationFilter = this.props.organizationFilter
 			if (organizationFilter) {
 				let positionType = filter.value.value || ""
-				if (positionType === "PRINCIPAL") {
+				if (positionType === Position.TYPE.PRINCIPAL) {
 					organizationFilter.setState({queryParams: {type: "PRINCIPAL_ORG"}})
-				} else if (positionType === "ADVISOR") {
+				} else if (positionType === Position.TYPE.ADVISOR) {
 					organizationFilter.setState({queryParams: {type: "ADVISOR_ORG"}})
 				} else {
 					organizationFilter.setState({queryParams: {}})

client/src/components/AssignPersonModal.js
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react'
 import autobind from 'autobind-decorator'
 import Autocomplete from 'components/Autocomplete'
 import {Modal, Button, Grid, Row, Col, Alert, Table} from 'react-bootstrap'
-import {Person} from 'models'
+import {Person, Position} from 'models'
 import API from 'api'
 
 export default class AssignPersonModal extends Component {
@@ -30,7 +30,7 @@ export default class AssignPersonModal extends Component {
 		let newPerson = this.state.person
 
 		let personSearchQuery = {}
-		if (position.type === 'PRINCIPAL') {
+		if (position.type === Position.TYPE.PRINCIPAL) {
 			personSearchQuery.role = 'PRINCIPAL'
 		} else  {
 			personSearchQuery.role = 'ADVISOR'

client/src/components/AssignPositionModal.js
@@ -36,19 +36,19 @@ export default class AssignPositionModal extends Component {
 
 		let positionSearchQuery = {}
 		if (person.role === 'ADVISOR') {
-			positionSearchQuery.type = ['ADVISOR']
+			positionSearchQuery.type = [Position.TYPE.ADVISOR]
 			if (currentUser.isAdmin()) { //only admins can put people in admin billets.
-				positionSearchQuery.type.push('ADMINISTRATOR')
-				positionSearchQuery.type.push('SUPER_USER')
+				positionSearchQuery.type.push(Position.TYPE.ADMINISTRATOR)
+				positionSearchQuery.type.push(Position.TYPE.SUPER_USER)
 			} else if (currentUser.isSuperUser()) {
 				//Only super users can put people in super user billets
 				//And they are limited to their organization.
-				positionSearchQuery.type.push('SUPER_USER')
+				positionSearchQuery.type.push(Position.TYPE.SUPER_USER)
 				positionSearchQuery.organizationId = currentUser.position.organization.id
 				positionSearchQuery.includeChildrenOrgs = true
 			}
 		} else if (person.role === 'PRINCIPAL') {
-			positionSearchQuery.type = ['PRINCIPAL']
+			positionSearchQuery.type = [Position.TYPE.PRINCIPAL]
 		}
 
 		return (

client/src/components/EditAssociatedPositionsModal.js
@@ -38,18 +38,18 @@ export default class EditAssociatedPositionsModal extends Component {
 		let {position} = this.props
 		let {associatedPositions} = this.state
 		let currentUser = this.context.currentUser
-		let assignedRole = position.type === 'PRINCIPAL' ? dict.lookup('ADVISOR_PERSON_TITLE') : dict.lookup('PRINCIPAL_PERSON_TITLE')
+		let assignedRole = position.type === Position.TYPE.PRINCIPAL ? dict.lookup('ADVISOR_PERSON_TITLE') : dict.lookup('PRINCIPAL_PERSON_TITLE')
 
 		let positionSearchQuery = {matchPersonName: true}
-		if (position.type === 'PRINCIPAL') {
-			positionSearchQuery.type = ['ADVISOR', 'SUPER_USER', 'ADMINISTRATOR']
+		if (position.type === Position.TYPE.PRINCIPAL) {
+			positionSearchQuery.type = [Position.TYPE.ADVISOR, Position.TYPE.SUPER_USER, Position.TYPE.ADMINISTRATOR]
 			if (currentUser.isAdmin() === false) {
 				//Super Users can only assign a position in their organization!
 				positionSearchQuery.organizationId = currentUser.position.organization.id
 				positionSearchQuery.includeChildrenOrgs = true
 			}
 		} else {
-			positionSearchQuery.type = ['PRINCIPAL']
+			positionSearchQuery.type = [Position.TYPE.PRINCIPAL]
 		}
 
 		return (

client/src/models/Person.js
@@ -4,6 +4,8 @@ import Model from 'components/Model'
 import utils from 'utils'
 import dict from 'dictionary'
 
+import {Position} from 'models'
+
 import RS_ICON from 'resources/rs_small.png'
 import AFG_ICON from 'resources/afg_small.png'
 
@@ -63,13 +65,13 @@ export default class Person extends Model {
 	}
 
 	isAdmin() {
-		return this.position && this.position.type === 'ADMINISTRATOR'
+		return this.position && this.position.type === Position.TYPE.ADMINISTRATOR
 	}
 
 	isSuperUser() {
 		return this.position && (
-			this.position.type === 'SUPER_USER' ||
-			this.position.type === 'ADMINISTRATOR'
+			this.position.type === Position.TYPE.SUPER_USER ||
+			this.position.type === Position.TYPE.ADMINISTRATOR
 		)
 	}
 
@@ -85,8 +87,8 @@ export default class Person extends Model {
 	// - A super user for this orgs parents.
 	isSuperUserForOrg(org) {
 		if (!org) { return false }
-		if (this.position && this.position.type === 'ADMINISTRATOR') { return true }
-		if (this.position && this.position.type !== 'SUPER_USER') { return false }
+		if (this.position && this.position.type === Position.TYPE.ADMINISTRATOR) { return true }
+		if (this.position && this.position.type !== Position.TYPE.SUPER_USER) { return false }
 		if (org.type === 'PRINCIPAL_ORG') { return true }
 
 		if (!this.position || !this.position.organization) { return false }

commit 58e1d30dda88c83105f97b4274955bb362411fc5

Fix quoting error introduced in commit 11a734cf064e4bb978c456b381c7648077aa4c26

client/src/pages/positions/Form.js
@@ -77,8 +77,8 @@ export default class PositionForm extends ValidatableFormWrapper {
 				<Fieldset title={edit ? `Edit Position ${position.name}` : "Create a new Position"}>
 					<Form.Field id="type" disabled={this.props.edit}>
 						<ButtonToggleGroup>
-							<Button id="typeAdvisorButton" value="{Position.TYPE.ADVISOR}">{dict.lookup('ADVISOR_POSITION_NAME')}</Button>
-							<Button id="typePrincipalButton" value="{Position.TYPE.PRINCIPAL}">{dict.lookup('PRINCIPAL_POSITION_NAME')}</Button>
+							<Button id="typeAdvisorButton" value={Position.TYPE.ADVISOR}>{dict.lookup('ADVISOR_POSITION_NAME')}</Button>
+							<Button id="typePrincipalButton" value={Position.TYPE.PRINCIPAL}>{dict.lookup('PRINCIPAL_POSITION_NAME')}</Button>
 						</ButtonToggleGroup>
 					</Form.Field>
 
@@ -117,10 +117,10 @@ export default class PositionForm extends ValidatableFormWrapper {
 					{position.type !== Position.TYPE.PRINCIPAL &&
 						<Form.Field id="permissions">
 							<ButtonToggleGroup>
-								<Button id="permsAdvisorButton" value="{Position.TYPE.ADVISOR}">{dict.lookup('ADVISOR_POSITION_TYPE_TITLE')}</Button>
-								<Button id="permsSuperUserButton" value="{Position.TYPE.SUPER_USER}">{dict.lookup('SUPER_USER_POSITION_TYPE_TITLE')}</Button>
+								<Button id="permsAdvisorButton" value={Position.TYPE.ADVISOR}>{dict.lookup('ADVISOR_POSITION_TYPE_TITLE')}</Button>
+								<Button id="permsSuperUserButton" value={Position.TYPE.SUPER_USER}>{dict.lookup('SUPER_USER_POSITION_TYPE_TITLE')}</Button>
 								{isAdmin &&
-									<Button id="permsAdminButton" value="{Position.TYPE.ADMINISTRATOR}">{dict.lookup('ADMINISTRATOR_POSITION_TYPE_TITLE')}</Button>
+									<Button id="permsAdminButton" value={Position.TYPE.ADMINISTRATOR}>{dict.lookup('ADMINISTRATOR_POSITION_TYPE_TITLE')}</Button>
 								}
 							</ButtonToggleGroup>
 						</Form.Field>