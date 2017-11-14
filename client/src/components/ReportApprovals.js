b61c4f82bbd3cba25d99bbf9ee2a7195bb222bce
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-11-14 11:40:36 +0100

    NCI-Agency/anet#179 Add time to approval state details view in report view

--- client/src/components/ReportApprovals.js
+++ client/src/components/ReportApprovals.js
@@ -93,7 +93,10 @@ export default class ReportApprovals extends Component {
             return(
                 <div className="approval-details">
                     <span>By {action.person.name}</span><br/>
-                    <small>On {moment(action.createdAt).format('D MMM YYYY, h:mm a')}</small>
+                    <small>
+                        On {moment(action.createdAt).format('D MMM YYYY')}<br/>
+                        At {moment(action.createdAt).format('h:mm a')}
+                    </small>
                 </div>
             )
         }
