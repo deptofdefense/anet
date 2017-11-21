7f0fddac44679d01881919b52547649410a306d2
Mara Dragan <mara@west.nl>
2017-11-21 14:05:58 +0100

    NCI-Agency/anet#278: Change title and description of some insights
    This changes the title and description of the PendigApprovalReports and
    CancelledEngagementReports insights in order to make them better
    understandable.

--- client/src/components/PendingApprovalReports.js
+++ client/src/components/PendingApprovalReports.js
@@ -50,10 +50,10 @@ export default class PendingApprovalReports extends Component {
     const focusDetails = this.focusDetails
     return (
       <div>
-        <p className="help-text">{`Number of reports pending approval since ${this.referenceDateLongStr}, grouped by advisor organization`}</p>
+        <p className="help-text">{`Number of pending reports, submited on or before ${this.referenceDateLongStr}, by advisor organization`}</p>
         <p className="chart-description">
           {`Displays the number of pending approval reports which have been
-            submitted up till ${this.referenceDateLongStr}. The reports are
+            submitted on or before ${this.referenceDateLongStr}. The reports are
             grouped by advisor organization. In order to see the list of
             pending approval reports for an organization, click on the bar
             corresponding to the organization.`}
